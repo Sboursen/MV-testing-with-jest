9e720b0b6a2478ea8a8f703acffca1775a060450
sboursen <dev.sboursen@gmail.com>
2022-02-22 16:51:38 +0100

    Write multiply methode

--- task3.js
+++ task3.js
@@ -32,6 +32,23 @@ class Calculator {
 
     return x - y;
   }
+
+  multiply(x, y) {
+    console.log(arguments);
+    Object.keys(arguments).forEach((arg) => {
+      if (arguments[arg] === undefined) {
+        throw new Error(`ARG ${arg} IS UNDEFINED`);
+      }
+    });
+
+    Object.keys(arguments).forEach((arg) => {
+      if (isNaN(arguments[arg])) {
+        throw new Error(`ARG ${arg} IS NAN`);
+      }
+    });
+
+    return x * y;
+  }
 }
 
 module.exports = Calculator;
